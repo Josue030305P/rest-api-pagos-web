0b29ae5f084f8b891d985e3e48f4a8cf09afb35c
Josue <josueyataco96@gmail.com>
2025-05-30 13:33:20 -0500

    Se arrgelo la ruta

--- src/app.js
+++ src/app.js
@@ -5,7 +5,11 @@ const app = express();
 app.use(express.json());
 app.use('api/', beneficiariosRoutes) ;
 
-
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: 'Ruta no encontrada'
+  });
+});
 
 
 
--- src/routes/beneficiarios.routes.js
+++ src/routes/beneficiarios.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getBeneficiarios } from "../controllers/beneficiarios.controller";
+import { getBeneficiarios } from "../controllers/beneficiarios.controller.js";
 const router = Router();
 
 router.get('/beneficiarios',getBeneficiarios);
